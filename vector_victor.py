39cada82b160d2a0e8388f3bf792fc761b4c3cc1
TracySanFilipo <tracysanfilipo@gmail.com>
2016-11-03 02:41:24 -0400

    Advanced, but still one place in normal could not raise ShapeError

--- vector_victor.py
+++ vector_victor.py
@@ -3,10 +3,14 @@ class ShapeError(Exception):
     pass
 
 def shape(vectors):
-    #columns = [len(col) for col in vectors if type(vectors[0]) == type([])]
-    row = len(vectors)
-    tuple1 = (row,)
-    return tuple1
+    if type(vectors[0]) == type([]):
+        columns = len(vectors[0])
+        row = len(vectors)
+        tuple1 = (row, columns)
+        return tuple1
+    else:
+        row = len(vectors)
+        return (row,)
 
 def vector_add(vector1, vector2):
     if shape(vector1) == shape(vector2):
@@ -51,3 +55,45 @@ def magnitude(vector1):
     squares = [item**2 for item in vector1]
     roots = math.sqrt(sum(squares))
     return roots
+
+def matrix_row(matrix1, index1):
+    row_of_matrix = matrix1[index1]
+    return row_of_matrix
+
+def matrix_col(matrix2, index2):
+    list_of_number_of_rows = range(len(matrix2))
+    col_of_matrix = [matrix2[number2][index2] for number2 in list_of_number_of_rows]
+    return col_of_matrix
+
+def matrix_add(matrix1, matrix2):
+    if shape(matrix1)[0] == shape(matrix2)[0] and shape(matrix1)[1] == shape(matrix2)[1]:
+        combo = [vector_add(vec1, vec2) for vec1, vec2 in zip(matrix1, matrix2)]
+        return combo
+    else:
+        raise ShapeError
+
+def matrix_sub(matrix1, matrix2):
+    if shape(matrix1)[0] == shape(matrix2)[0] and shape(matrix1)[1] == shape(matrix2)[1]:
+        combo = [vector_sub(vec1, vec2) for vec1, vec2 in zip(matrix1, matrix2)]
+        return combo
+    else:
+        raise ShapeError
+
+def matrix_scalar_multiply(matrix1, scalar1):
+    msm = [vector_multiply(row, scalar1) for row in matrix1]
+    return msm
+
+def matrix_vector_multiply(matrix1, vector1):
+    if shape(matrix1)[1] == shape(vector1)[0]:
+        combo = [sum([item * thing for item, thing in zip(row, vector1)]) for row in matrix1]
+        return combo
+    else:
+        raise ShapeError
+
+def matrix_matrix_multiply(matrix1, matrix2):
+    if shape(matrix1)[1] == shape(matrix2)[0]:
+        col_list = range(shape(matrix2)[1])
+        combo = [[sum([item * thing[index3] for item, thing in zip(row, matrix2)]) for index3 in col_list] for row in matrix1]
+        return combo
+    else:
+        raise ShapeError
